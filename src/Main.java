68ed8cbe8d33f893c2f56331b8af6ce74369a85c
taeyeongmin <alsxodud123@naver.com>
2023-12-18 12:27:08 +0900

    binarySearch

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
+import java.util.Scanner;
+
+import static chapter1.part2.piramid.printPiramid;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
+
+//    public static void main(String[] args) {
+//
+//        Scanner sc = new Scanner(System.in);
+//
+//        System.out.println("몇 단의 별을 출력할지 입력");
+//        int no = sc.nextInt();
+//        sc.close();
+//        printPiramid(no);
+//    }
 }
--- src/part1/MaxValue.java
+++
@@ -1,2 +0,0 @@
-package part1;public class MaxValue {
-}
